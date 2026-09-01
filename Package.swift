23336ff1e833901bfac6895d1b4b110a2fc04f8a
Alexandre H. Saad <alexandresaad@icloud.com>
2026-09-01 20:29:52 +0100

    build!: require Swift 6.3

--- Package.swift
+++ Package.swift
@@ -1,47 +1,44 @@
-// swift-tools-version:5.9
-
-// This source file is part of the Measures open source project
-//
-// Copyright (c) 2021-2026 A. H. de Quatre Ltd. and the Measures project authors
-// Licensed under Apache License v2.0 with Runtime Library Exception
-//
-// See LICENSE.md for license information
-// See CONTRIBUTORS.txt for the list of Measures project authors
+// swift-tools-version:6.3
 
 import PackageDescription
 
-let package: Package = .init(
+let package = Package(
     name: "swift-measures",
     defaultLocalization: "en",
     products: [
-        .library(name: "Measures", targets: ["Measures"])
+        .library(
+            name: "Measures",
+            targets: [
+                "Measures"
+            ]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/bikecalc/swift-numerics-extended.git", from: "2.0.0"),
-        .package(url: "https://github.com/swiftlang/swift-docc-plugin.git", from: "1.4.2")
+        .package(
+            url: "https://github.com/bikecalc/swift-numerics-extended.git",
+            from: "2.0.0"
+        ),
+        .package(
+            url: "https://github.com/swiftlang/swift-docc-plugin.git",
+            from: "1.4.2"
+        )
     ],
     targets: [
         .target(
             name: "Measures",
             dependencies: [
-                .product(name: "NumericsExtended", package: "swift-numerics-extended")
-            ],
-            path: "Sources",
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency=complete")
+                .product(
+                    name: "NumericsExtended",
+                    package: "swift-numerics-extended"
+                )
             ]
         ),
         .testTarget(
             name: "MeasuresTests",
-            dependencies: ["Measures"],
-            path: "Tests",
-            resources: [
-                .process("MeasuresTests/Resources")
-            ],
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency=complete")
+            dependencies: [
+                "Measures"
             ]
         )
     ],
-    swiftLanguageVersions: [.v5]
+    swiftLanguageModes: [.v6]
 )
