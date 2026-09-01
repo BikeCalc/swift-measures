c47ef2dff413452b2640b3104d8ee58e73de1730
Alexandre H. Saad <alexandresaad@icloud.com>
2026-09-01 20:20:20 +0100

    chore: update Swift file headers

--- Package.swift
+++ Package.swift
@@ -1,5 +1,13 @@
 // swift-tools-version:5.9
 
+// This source file is part of the Measures open source project
+//
+// Copyright (c) 2021-2026 A. H. de Quatre Ltd. and the Measures project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See LICENSE.md for license information
+// See CONTRIBUTORS.txt for the list of Measures project authors
+
 import PackageDescription
 
 let package: Package = .init(
--- Sources/Measures/Types/Units/BaseUnits/Memory.swift
+++ Sources/Measures/Types/Units/BaseUnits/Memory.swift
@@ -1,10 +1,10 @@
+// This source file is part of the Measures open source project
 //
-// Memory.swift
-// Measures
-//
-// Copyright © 2021-2024 Alexandre H. Saad
+// Copyright (c) 2021-2026 A. H. de Quatre Ltd. and the Measures project authors
 // Licensed under Apache License v2.0 with Runtime Library Exception
 //
+// See LICENSE.md for license information
+// See CONTRIBUTORS.txt for the list of Measures project authors
 
 /// A unit of measure for memory.
 public struct Memory {
